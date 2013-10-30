9ae463d1896946db6bfeb26ef1480391f8345114
Daniel Jurczak <daniel.jurczak@imba.oeaw.ac.at>
2013-10-30 12:50:59 +0100

    showing placeholders for elements that lack plasmid_map, generating thumbs

--- app/models/plasmid.rb
+++ app/models/plasmid.rb
@@ -2,7 +2,7 @@ class Plasmid < ActiveRecord::Base
   attr_accessible :author_id, :info, :internal_id, :name, :plasmid_map, :attachments_attributes
   serialize :info, ActiveRecord::Coders::Hstore
 
-  has_attached_file :plasmid_map
+  has_attached_file :plasmid_map, styles: {thumb: ["400x400#", :png]}, :default_url => 'http://placehold.it/390x390'
 
   belongs_to :author
   has_many :attachments, :dependent => :destroy
